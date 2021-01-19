0817a87072137a82603f41202218b7be056be43f
Heidet <a.heidet9040@gmail.com>
2021-01-19 20:20:18 +0100

    ajout des icon fontawesome pour les skills + responsive + JS pour les skills

--- Django/store/static/store/js/skill.js
+++ Django/store/static/store/js/skill.js
@@ -2,13 +2,13 @@ var lang = {
     "html": "100%",
     "css": "90%",
     "javascript": "70%",
-    "python": "65%",
+    "python": "55%",
     "vue": "35%"
 };
   
 var multiply = 4;
 
-$.each( lang, function( language, pourcent) {
+$.each( lang, function(language, pourcent) {
 
 var delay = 700;
 
